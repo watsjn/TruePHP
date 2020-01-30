d8f2f10844ad2175260df711833d0d024be0ae35
watsjn <aliaksei.shumovich@gmail.com>
2020-01-30 20:15:59 +0100

    swiftmailer default "from" declaration

--- manager/src/Model/User/Service/ResetTokenSender.php
+++ manager/src/Model/User/Service/ResetTokenSender.php
@@ -11,10 +11,6 @@ use Twig\Environment;
 class ResetTokenSender
 {
     /**
-     * @var array
-     */
-    private $from;
-    /**
      * @var \Swift_Mailer
      */
     private $mailer;
@@ -23,17 +19,15 @@ class ResetTokenSender
      */
     private $twig;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
         $this->twig = $twig;
-        $this->from = $from;
     }
 
     public function send(Email $email, ResetToken $token): void
     {
         $message = (new \Swift_Message('Password resetting'))
-            ->setFrom($this->from)
             ->setTo($email->getValue())
             ->setBody($this->twig->render('mail/user/reset.html.twig', [
                 'token' => $token->getToken()
