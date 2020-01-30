cbaa936fd00b882b39ba06cd20bb5e8a804f397b
watsjn <aliaksei.shumovich@gmail.com>
2020-01-30 18:55:39 +0100

    Readmodel added

--- manager/src/Container/Model/User/Service/ResetTokenizerFactory.php
+++ manager/src/Container/Model/User/Service/ResetTokenizerFactory.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Container\Model\User\Service;
 
+use App\Model\User\Service\ResetTokenizer;
+
 class ResetTokenizerFactory
 {
     public function create(string $interval): ResetTokenizer
